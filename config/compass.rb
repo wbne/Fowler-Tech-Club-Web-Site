b7060e3f43c92e922a2a4a584d87c970ccd8b037
Pranoy Dutta <pranoydll@gmail.com>
2016-09-01 21:18:32 -0500

    made css compressed using config/compass.rb's output specification - Pranoy 9/1/2016

--- config/compass.rb
+++ config/compass.rb
@@ -9,7 +9,7 @@ images_dir = "imgs"
 javascripts_dir = "scripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+ output_style = :compressed # setting output style
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
