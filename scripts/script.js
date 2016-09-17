0807bcd9e786999a8a4bcfa9276a45d31e6eb7d7
Pranoy Dutta <pranoydll@gmail.com>
2016-09-16 20:37:59 -0500

    changed some UI - Pranoy Dutta 9/16/2016

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,25 @@
 function openNav() {
 	$('#mainSideNav').css("width", "250px");
+	$('#mainSideNav').css("opacity", "1");
+	$('#content-wrap').css("margin-left", "250px");
 }
 
 function closeNav() {
 	$("#mainSideNav").css("width", "0px");
+	$('#mainSideNav').css("opacity", "0");
+	$('#content-wrap').css("margin-left", "0px");
 }
 
-$(document).ready(function(){});
+$(document).ready(function(){
+	$('#playMusic').click(function(){
+		$('#music').trigger('play');
+		$(this).css('display', 'none');
+		$('#pauseMusic').css('display', 'block');
+	});
+
+	$('#pauseMusic').click(function(){
+		$('#music').trigger('pause');
+		$(this).css('display', 'none');
+		$('#playMusic').css('display', 'block');
+	});
+});
